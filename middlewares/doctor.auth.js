6a527eceb8ca210d5bb663ea8ad111e6803ca25b
dianaPermata <dianapermatap2120@gmail.com>
2022-11-17 22:23:16 -0800

    add file --validasi

--- middlewares/doctor.auth.js
+++ middlewares/doctor.auth.js
@@ -3,58 +3,58 @@ const jwt = require('jsonwebtoken');
 
 const doctorAuth = (req, res, next) => {
     const token = req.header('doctor-token')
-    if(!token) 
-    return res.status(403).json({
-        status : res.statusCode,
-        message : "Access Denied!"
-    }) 
-    try{
+    if (!token)
+        return res.status(403).json({
+            status: res.statusCode,
+            message: "Access Denied!"
+        })
+    try {
         const verified = jwt.verify(token, process.env.DOCTOR_KEY)
         // console.log(verified.doctor._id);
         if (!verified) {
             res.status(401).send({
-              message: "Unauthorized!",
+                message: "Unauthorized!",
             });
-          }else if(verified.doctor._id != req.params.id){
-          return res.status(403).json({
-              status : res.statusCode,
-              message : "Access Denied!"
-          }) //Checks doctor's id currently logged in is the same as req.params.id
+        } else if (verified.doctor._id != req.params.id) {
+            return res.status(403).json({
+                status: res.statusCode,
+                message: "Access Denied!"
+            }) //Checks doctor's id currently logged in is the same as req.params.id
         }
-          next()
-    }catch(error){
+        next()
+    } catch (error) {
         res.status(400).json({
-            status : res.statusCode,
-            message : "Invalid Token!"
+            status: res.statusCode,
+            message: "Invalid Token!"
         })
     }
 }
 
 const doctorAuthArticle = (req, res, next) => {
     const token = req.header('doctor-token')
-    if(!token) 
-    return res.status(403).json({
-        status : res.statusCode,
-        message : "Access Denied!"
-    }) 
-    try{
+    if (!token)
+        return res.status(403).json({
+            status: res.statusCode,
+            message: "Access Denied!"
+        })
+    try {
         const verified = jwt.verify(token, process.env.DOCTOR_KEY)
         // console.log(verified.doctor._id);
         if (!verified) {
             res.status(401).send({
-              message: "Unauthorized!",
+                message: "Unauthorized!",
             });
-          }else if(verified.doctor._id != req.params.id){
-          return res.status(403).json({
-              status : res.statusCode,
-              message : "Access Denied!"
-          }) //Checks doctor's id currently logged in is the same as req.params.id
+        } else if (verified.doctor._id != req.params.id) {
+            return res.status(403).json({
+                status: res.statusCode,
+                message: "Access Denied!"
+            }) //Checks doctor's id currently logged in is the same as req.params.id
         }
-          next()
-    }catch(error){
+        next()
+    } catch (error) {
         res.status(400).json({
-            status : res.statusCode,
-            message : "Invalid Token!"
+            status: res.statusCode,
+            message: "Invalid Token!"
         })
     }
 }
